be090f139d3be74c2491fbacf096fcb770f26a85
Jasontansz <67975812+Jasontansz@users.noreply.github.com>
2021-03-18 14:28:58 -0400

    Update NFT.sol

--- 01-Code/src/backEnd/contracts/NFT.sol
+++ 01-Code/src/backEnd/contracts/NFT.sol
@@ -11,7 +11,7 @@ contract NFT is ERC721, Ownable {
 
   event Purchase(address owner, uint price, uint id, string uri);
 
-  constructor() ERC721("Dapp University", "DAPPU") {
+  constructor() ERC721("Creative Art NFT", "CAN") {
   	_owner = msg.sender;
   }
 
